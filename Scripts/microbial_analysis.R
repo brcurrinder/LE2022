370e9f8901022c51a45bedc1115088c558fae3ec
Anna <anna.schmidt@uvm.edu>
2023-02-10 14:53:59 -0500

    added random forest analysis steps to the end of this script

--- Scripts/microbial_analysis.R
+++ Scripts/microbial_analysis.R
@@ -38,7 +38,7 @@ data_long <- gather(data, date, abund, "23Apr2001":"24Apr2019")
 data_summed <- data_long %>% 
   group_by(date, phylum) %>%
   summarise(abund =  sum(abund, na.rm = TRUE)) %>%
-  ungroup() 
+  ungroup()
 
 # Remove dates that have an extension after the date -- check in with Robin about this -- I am pretty sure these are non-standard samples that should be removed
 data_summed <- data_summed[!grepl("\\.", data_summed$date), ]
@@ -75,7 +75,8 @@ cyano %>%
 
 # Write .csv for time series figure
 rel_abund_cyano <- cyano %>%
-  select(-c(date, phylum, sampledate, day, month)) %>%
+  #select(-c(date, phylum, sampledate, day, month)) %>%
+  select(-c(phylum)) %>%
   rename("Cyanobacteria_rel_abund_16S" = "abund")
 write.csv(rel_abund_cyano, "Data/prelim_time_series_figure/Cyanobacteria_rel_abund_16S.csv", row.names = F, quote=T)
 
@@ -103,3 +104,99 @@ data_summed_subset %>%
         legend.text=element_text(size=12),
         title=element_text(size=14),
         legend.title=element_text(size=14,face="bold"))
+
+
+#----------------------------------------------------------------------
+# For random forest analysis
+#----------------------------------------------------------------------
+
+library(randomForest)
+library(pdp)
+
+data_summed2 <- data_summed %>%
+  select(-c(date, day, month, year, doy)) 
+
+summarized <- data_summed2 %>%
+  pivot_wider(names_from = phylum, values_from = abund)
+
+metabolism <- read.csv("Data/Metabolism_Model_Output/SimResultsMatrix_MetabData_run12jan23.csv") # load metab data
+
+metabolism <- metabolism %>% rename("sampledate" = "SimDate")
+
+metabolism$sampledate <- as.Date(metabolism$sampledate)
+
+rf_input <- merge(x = summarized, y = metabolism, by.x = "sampledate")
+
+rf_input <- rf_input %>%
+  select(1:73, 78) %>%
+  select(-contains("-")) # columns with dashes were causing us trouble
+
+# Create training and test sets
+rf_training <- sample_frac(tbl=rf_input,size=0.7,replace=FALSE)
+rf_test <- rf_input[!(rf_input$sampledate %in% rf_training$sampledate),]
+
+# Remove the date from the training set
+rf_training <- rf_training %>% select(-c(sampledate))
+
+# Run the random forest
+rf <- randomForest(data=rf_training, EpiNPP_mgC_L~., localImp=TRUE, err.rate=TRUE)
+
+rf
+plot(rf) # what it's supposed to look like
+
+# Plot predicted vs. observed NPP
+rf_test$predict <- predict(rf, rf_test)
+rf_test %>%
+  ggplot(aes(x= EpiNPP_mgC_L, y = predict)) +
+  geom_point() +
+  geom_abline()
+
+# Variable importance plot
+varImpPlot(rf)
+# node purity - not many people use
+# %IncMSE - % increase in MSE if that variable is not included
+
+# Partial dependence plot - how the prediction responds in relation to one of the variables
+pd <- partial(object = rf,
+              pred.var = 'Spirochaetota',
+              plot = TRUE)
+# the yhat (metabolism) goes up steeply with any presence of Spiro
+pd <- partial(object = rf,
+              pred.var = 'Cyanobacteria',
+              plot = TRUE)
+pd <- partial(object = rf,
+              pred.var = 'Bacteroidota',
+              plot = TRUE)
+pd <- partial(object = rf,
+              pred.var = 'Planctomycetota',
+              plot = TRUE)
+
+# Run actual regression and see what the r-squared is
+model <- lm(data = rf_test, formula = predict ~ EpiNPP_mgC_L)
+
+summary(model)
+# pretty good!
+
+
+
+#--------------------------------------------------------------
+# Plot trends in four desired phyla over time
+#-------------------------------------------------------------
+
+# Subset
+fourphyla <- subset(data_summed, phylum %in% c("Cyanobacteria", "Spirochaetota", "Planctomycetota", "Bacteroidota"))
+
+fourphyla$year <- as.character(fourphyla$year)
+fourphyla$phylum <- factor(fourphyla$phylum,
+                           levels = c("Spirochaetota",
+                                      "Cyanobacteria",
+                                      "Planctomycetota",
+                                      "Bacteroidota"))
+
+# Plot with a different line for each year, faceted by phylum
+fourphyla %>%
+  ggplot(aes(x = doy, y = abund, col = year)) + 
+  geom_line(stat = "identity") +
+  facet_wrap(~phylum, ncol = 1, scales = "free_y") +
+  theme_classic() +
+  labs(x = "Day of year", y = "Phylum relative abundance (%)", title = "")
